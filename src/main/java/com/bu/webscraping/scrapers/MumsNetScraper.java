a16127f1872e99ffc5de1b0b5141edb9ff9d78c0
Gregam3 <gregoryamitten@gmail.com>
2018-09-17 15:12:24 +0100

    Fixed minor bug where forum type was missing leading to npe

--- src/main/java/com/bu/webscraping/scrapers/MumsNetScraper.java
+++ src/main/java/com/bu/webscraping/scrapers/MumsNetScraper.java
@@ -1,5 +1,7 @@
 package com.bu.webscraping.scrapers;
 
+import com.bu.forum.ForumType;
+
 /**
  * @author Greg Mitten (i7676925)
  * gregoryamitten@gmail.com
@@ -12,6 +14,6 @@ public class MumsNetScraper extends AbstractScraper {
         setPageUrlPrefix("?pg=");
         setPostGroupIndexes(new int[]{3,1,2});
 
-
+        setForumType(ForumType.MUMS_NET);
     }
 }
--- src/main/java/com/bu/webscraping/scrapers/NetMumsScraper.java
+++ src/main/java/com/bu/webscraping/scrapers/NetMumsScraper.java
@@ -1,5 +1,7 @@
 package com.bu.webscraping.scrapers;
 
+import com.bu.forum.ForumType;
+
 /**
  * @author Greg Mitten (i7676925)
  * gregoryamitten@gmail.com
@@ -14,5 +16,7 @@ public class NetMumsScraper extends AbstractScraper {
         setPageUrlSuffix(".html");
         setPostGroupIndexes(new int[]{3,2,1});
         setRequiresHTMLExtension(true);
+
+        setForumType(ForumType.NET_MUMS);
     }
 }
